585c798d2ea1b1c918e93d1efd86f9999fb1c48c
stjohnd <devon.t.st.john@gmail.com>
2018-05-31 22:34:48 -0400

    Pings
    Ping request and response now works. However, this behavior becomes undefined when pinging a broadcast address. This finishes half of the required functions, leaving only route discovery, dependant on Routers

--- GERTi/lib/icmp.lua
+++ GERTi/lib/icmp.lua
@@ -3,26 +3,34 @@ local event = require("event")
 
 local icmp = {}
 
---
-icmp.ping_target = function(source,dest,data)
-  if not data then
-    data = "010123456789ABCDEF"
-  else
-    data = "01"..data
-  end
+icmp.ping_target = function(source,dest,data,timeout)
+  if not data then data = "010123456789ABCDEF" else data = "01"..data end
+  if not timeout then timeout = 5 end
   
+  GERTi.send(source,dest,1,data)
+  
+  reply = event.pull(timeout,"gert_packet",source,dest,1,"02"..data:sub(3))
+  if reply then return true else return false end
 end
 
-icmp.ping = function(dest,data)
-  if not data then
-    data = "010123456789ABCDEF"
-  else
-    data = "01"..data
-  end
+icmp.ping = function(dest,data,timeout)
+  if not data then data = "010123456789ABCDEF" else data = "01"..data end
+  if not timeout then timeout = 5 end
+  
+  GERTi.send(dest,1,data)
+
+  reply = event.pull(timeout,"gert_packet",nil,dest,1,"02"..data:sub(3))
+  if reply then return true else return false end
 end
 
 local function onPacket(name,source,dest,proto,data)
   if proto ~= 1 then return end
   
-  
+  if data:sub(1,2) == "01" then
+    GERTi.send(dest,1,"02"..data:sub(3))
+  end
 end
+
+event.listen("gert_packet",onPacket)
+
+return icmp
