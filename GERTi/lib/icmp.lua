343700e6ea6066693702e581ed9b749020ada638
stjohnd <devon.t.st.john@gmail.com>
2018-06-01 14:55:37 -0400

    Roughing in route discovery
    Testing will need to be done upon creation of the router program

--- GERTi/lib/icmp.lua
+++ GERTi/lib/icmp.lua
@@ -1,5 +1,6 @@
 local gert = require("GERTi")
 local event = require("event")
+local serial = require("serialization")
 
 local icmp = {}
 
@@ -21,6 +22,28 @@ icmp.ping = function(dest,data,timeout)
 
   reply = event.pull(timeout,"gert_packet",nil,dest,1,"02"..data:sub(3))
   if reply then return true else return false end
+end
+
+--replies should be formatted as "04{"XXXXXX/XXXXXX"=XXXXXX
+icmp.find_routes(interface)
+  interface:send(GERTi.utils.getBroadcastAddr(interface),1,"03")
+
+  local scanning = true
+  event.timer(5,function() event.push("gert_scan_end") end)
+
+  while scanning do
+    local t = {event.pull("gert")}
+    if t[1] == "gert_scan_end" then
+      scanning = false
+    elseif t[1] == "gert_packet" and t[2] == interface.addr and t[4] == 2 and t[5]:sub(1,2) == "04" them
+      r = serial.unserialize(t[5]:sub(3))
+      for k,v in pairs(r) do
+        if not GERTi.routes[k] then
+          GERTi.routes[k] = v
+        end
+      end
+    end
+  end
 end
 
 local function onPacket(name,source,dest,proto,data)
