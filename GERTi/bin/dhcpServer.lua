b91c0e70e8bbbdb8d30e09a85539021514bb0d46
stjohnd <devon.t.st.john@gmail.com>
2018-06-01 14:15:26 -0400

    Basic DHCP server
    Requires testing, expansion could also be done to allow releasing of addresses

--- GERTi/bin/dhcpServer.lua
+++ GERTi/bin/dhcpServer.lua
@@ -5,7 +5,9 @@ local serial = require("serialization")
 --Usage: interface network subnet start end
 
 local tArgs = {...}
-local network, subnet, start, current end
+local network, subnet, start, peak
+
+local alloc = {}
 
 if not type(tArgs[1]) == "string" or #tArgs ~= 5 or tArgs[1] == "--help" or tArgs[1] == '?' then
   printUsage()
@@ -15,24 +17,36 @@ end
 network = tonumber(tArgs[2],16)
 subnet = tonumber(tArgs[3],16)
 start = tonumber(tArgs[4],16)
-current = start
-end = tonumber(tArgs[5],16)
+peak = tonumber(tArgs[5],16)
 
 local function printUsage()
   print("Usage: interface network subnet start end\nnetwork, subnet, start, and end must all be hexidecimal")
 end
+
+local function nextFree(){
+  for i = start, i < end, 1 do
+    if not alloc[i] then
+      return i
+    end
+  end
+  return -1
+}
 
 local function handleRequest(name,us,them,proto,data)
   if proto ~= 2 or us ~= gert.interfaces[interface].addr or data ~= "D" then
     return
   end
   
-  gert.interfaces[interface]:send(them,2,"O"..serial.serialize({addr = current,subnet = subnet}))
-  current = current + 1
-  acceptance = {event.pull(5,"gert_packet",them,us,2,string.format("R%06X",current-1))}
+  addr = nextFree()
+  
+  alloc[addr] = true
+  
+  gert.interfaces[interface]:send(them,2,"O"..serial.serialize({addr = network | addr,subnet = subnet}))
+  
+  acceptance = {event.pull(5,"gert_packet",them,us,2,string.format("R%06X",network | addr))}
   if not acceptance then return end
   
-  gert.interfaces[interface]:send(them,2,"O"..serial.serialize({addr = current,subnet = subnet}))
+  gert.interfaces[interface]:send(them,2,"A")
 end
 
 event.listen("gert_packet",handleRequest)
