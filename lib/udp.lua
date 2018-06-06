fa80f27d3f536149f4cd7db641c4dd62764df8eb
stjohnd <devon.t.st.john@gmail.com>
2018-06-06 10:50:33 -0400

    Update udp.lua
    New Encoding Scheme, more bugfixes, general refinement

--- lib/udp.lua
+++ lib/udp.lua
@@ -5,12 +5,23 @@ local gert = require("GERTi")
 local bufferMax = 65535
 
 local udp = {
-bufferMax = bufferMax,
+  bufferMax = bufferMax,
 }
 
 --All opened sockets are stored here, this is intentionally hidden
 --Keys are arranged in a map, formatted as DDDDDDIIII where D is Dest and I is ID
 local connections = {}
+
+--Given 2 ints between 0 and 65535, returns a string that can be converted back
+local function buildHeader(id,length)
+  return string.char(math.floor(id/256),id&255,math.floor(length/256),length&255)
+end
+
+--Given a string in which the first 4 characters represent 2 ints, returns the ints themselves
+local function breakHeader(header)
+  local imsb, ilsb, lmsb, llsb = string.byte(header,1,4)
+  return ((imsb*256)+ilsb), ((lmsb*256)+llsb)
+end
 
 --[[Basic send function. useful for quick packets
 dest: The destination IP Address
@@ -19,12 +30,12 @@ id: If provided, will use this instead
 ]]
 function udp.send(dest,data,id)
   if not id then id = math.floor(math.random(0,65535)) end
-  gert.send(dest,8,string.format("%04X",id)..data)
+  gert.send(dest,8,buildHeader(id,#data)..data)
 end
 
---Write data to a socket
+--Write data to a socket, wrapper for basic send function
 local function swrite(self,data)
-  gert.send(self.dest,8,string.format("%04X",self.id)..data)
+  udp.send(self.dest,data,self.id)
 end
 
 --Read data from a socket
@@ -32,8 +43,10 @@ local function sread(self)
   if #self.readBuffer == 0 then
     event.pull("udp_packet",self.dest,self.id)
   end
-  returnBuffer = self.readBuffer
-  self.readBuffer = ""
+  local m,l = string.byte(self.readBuffer,1,2)
+  local length = (m*256)+l
+  returnBuffer = self.readBuffer:sub(length)
+  self.readBuffer = self.readBuffer:sub(length + 1, #self.readBuffer)
   return returnBuffer
 end
 
@@ -47,16 +60,24 @@ dest: The destination IP address
 responder: NOT YET IMPLEMENTED
 id: The id of the connection, used to open a reply socket or force an id
 ]]
-function udp.openSocket(dest,responder,id)
+function udp.openSocket(dest,id,responder)
   --Enforce application's right to exclusive access to a socket
-  if connections[string.format("%06X%04X",dest,id] then
+  if connections[string.format("%06X%04X",dest,id)] then
     return false, "Socket already opened"
   end
   
-  --Avoid connection collisions
-  repeat  
-  id = math.floor(math.random(0,65535))      
-  until not connections[string.format("%06X%04X",dest,id)]
+  local connKey
+  
+  --Since uniqueness was enforced above, if we were provided an ID, generate the Connection Key
+  if id then
+    connKey = string.format("%06X%04X",dest,id)
+  else
+    --Avoid connection collisions
+    repeat  
+      id = math.floor(math.random(0,65535))
+      connKey = string.format("%06X%04X",dest,id)
+    until not connections[connKey]
+  end
     
   local socket = {
     dest = dest,
@@ -68,7 +89,7 @@ function udp.openSocket(dest,responder,id)
     close = sclose,
   }
   
-  connections[string.format("%06X%04X",dest,id] = socket
+  connections[connKey] = socket
   
   return socket
 end
@@ -77,12 +98,13 @@ end
 local function udpReceive(name,receiver,sender,proto,data)
   if proto ~= 8 then return end
   
-  conID = tonumber(data:sub(1,4),16)
-  data = data:sub(5)
+  
+  connID, length = breakHeader(data) --Extract the Connection ID, length, and data from the packet
+  data = data:sub(3,#data) --We leave the length header on for now though, we may need it later.
   
   --If a socket is open for this connection, buffer the packet in it
-  if connections[string.format("%06X%04X",sender,conID] then
-    local conn = connections[string.format("%06X%04X",sender,conID]
+  if connections[string.format("%06X%04X",sender,connID)] then
+    local conn = connections[string.format("%06X%04X",sender,connID)]
     if #conn.readBuffer + #data > bufferMax then
       conn.bufferOverrun = true --Buffer exceeded flag is set and data is discarded
     else
@@ -93,7 +115,8 @@ local function udpReceive(name,receiver,sender,proto,data)
   end
   
   
-  event.push("udp_packet",sender,conID,data) --Connection-less packets have their data in the event
+  event.push("udp_packet",sender,conID,data:sub(3,#data)) --Connection-less packets have their data in the event
+  --we also strip the length header
 end
 
 event.listen("gert_packet",udpReceive)
